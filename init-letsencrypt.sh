163d3667e58b1ad9a4f4edde332c926814912b83
joan <joan.herisson@univ-evry.fr>
2020-01-21 09:17:51 +0100

    add xtms

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(galaxy-synbiocad.org)
+domains=(galaxy-synbiocad.org xtms.micalis.inrae.fr)
 rsa_key_size=4096
 #data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
