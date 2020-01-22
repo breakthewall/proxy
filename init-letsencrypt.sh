5f5477aca0551bb91a039a6d74467caada379956
joan <joan.herisson@univ-evry.fr>
2020-01-22 12:03:40 +0100

    add e-mail

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(galaxy-synbiocad.org synbiocad.micalis.inra.fr xtms.micalis.inrae.fr)
 rsa_key_size=4096
 #data_path="./certbot"
-email="" # Adding a valid address is strongly recommended
+email="joan.herisson@univ-evry.fr" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 #if [ -d "$data_path" ]; then
