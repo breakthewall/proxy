8860ff15fccfb1c2429f1d4c6e20f86b37de6130
joan <joan.herisson@univ-evry.fr>
2020-05-06 12:28:42 +0200

    rm comments

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,15 +9,7 @@ read -p 'Please enter your FQDN to generate the certificate: ' domain
 read -p 'Please enter your email: ' email
 #domains=(galaxy-synbiocad.org synbiocad.micalis.inra.fr xtms.micalis.inrae.fr)
 rsa_key_size=4096
-#data_path="./certbot"
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
-
-#if [ -d "$data_path" ]; then
-#  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-#  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-#    exit
-#  fi
-#fi
 
 
 docker-compose run --rm init-letsencrypt sh -c "\
